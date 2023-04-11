f09b9957c4c391032a97c5ac4e15c2b0aaba1b52
DrewPMiller <drewpmiller@berkeley.edu>
2023-04-11 14:01:31 -0700

    config.py, sound.py, uploaded move and capture sounds

---
+++ src/config.py
@@ -0,0 +1,13 @@
+import pygame
+import os
+
+from sound import Sound
+
+
+class Config:
+
+    def __init__(self):
+        self.move_sound = Sound(
+            os.path.join('assets/sounds/move.wav'))
+        self.capture_sound = Sound(
+            os.path.join('assets/sounds/capture.wav'))
