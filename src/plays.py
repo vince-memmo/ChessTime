7bd7d7c8ee8a8f6be9a80df502eecdd962867fb4
Vince Memmo <vmemmo1@gmail.com>
2023-05-01 21:54:48 -0700

    plays file

---
+++ src/plays.py
@@ -0,0 +1,24 @@
+from const import *
+from board import Board
+from dragger import Dragger
+from square import Square
+
+class Game:
+
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
+        pygame.display.set_caption('Chess')
+        self.board = Board()
+        self.dragger = Dragger()
+
+    def show_bg(self, surface):
+        for row in range(ROWS):
+            for col in range(COLS):
+                if (row + col) % 2 == 0:
+                    color = (174, 137, 104)
+                else:
+                    color = (236, 218, 185)
+                rect = (col * SQSIZE, row * SQSIZE, SQSIZE, SQSIZE)
+
+                pygame.draw.rect(surface, color, rect)
