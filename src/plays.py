44790d950ae9bce0b2c22308904f232172a06dc4
Vince Memmo <vmemmo1@gmail.com>
2023-05-01 22:23:38 -0700

    add moves to plays

--- src/plays.py
+++ src/plays.py
@@ -22,3 +22,33 @@ class Game:
                 rect = (col * SQSIZE, row * SQSIZE, SQSIZE, SQSIZE)
 
                 pygame.draw.rect(surface, color, rect)
+
+
+    def show_pieces(self, surface):
+        for row in range(ROWS):
+            for col in range(COLS):
+
+                # print(row, col != 1, 0)
+                if self.board.squares[row][col].has_piece():
+                    piece = self.board.squares[row][col].piece
+
+                    if piece is not self.dragger.piece:
+                        piece.set_texture(size=80)
+                        img = pygame.image.load(piece.texture)
+                        img_center = col * SQSIZE + SQSIZE // 2, row * SQSIZE + SQSIZE // 2
+                        piece.texture_rect = img.get_rect(center=img_center)
+                        surface.blit(img, piece.texture_rect)
+
+    def show_moves(self, surface):
+        if self.dragger.dragging:
+            piece = self.dragger.piece
+            for move in piece.moves:
+                color = '#808080'
+                x = move.final.col * SQSIZE + (SQSIZE // 2)
+                y = move.final.row * SQSIZE + (SQSIZE // 2)
+                rect = (move.final.col * SQSIZE, move.final.row * SQSIZE, SQSIZE, SQSIZE)
+                pygame.draw.circle(surface, color, (x, y), 20, 5)
+
+    # def show_moves1(self, surface):
+    #     self.piece.set_texture()
+    #     texture = self.piece.texture
