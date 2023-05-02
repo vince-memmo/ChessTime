ecbae796c57262762d1ab3f002f6605e73dfa5ab
Vince Memmo <vmemmo1@gmail.com>
2023-05-01 23:16:21 -0700

    debugging plays

--- src/plays.py
+++ src/plays.py
@@ -49,6 +49,12 @@ class Game:
                 rect = (move.final.col * SQSIZE, move.final.row * SQSIZE, SQSIZE, SQSIZE)
                 pygame.draw.circle(surface, color, (x, y), 20, 5)
 
-    # def show_moves1(self, surface):
-    #     self.piece.set_texture()
-    #     texture = self.piece.texture
+    def set_texture(self, size=80):
+        self.texture = os.path.join(
+            f'../assets/images/imgs-{size}px/{self.color}_{self.name}.png')
+
+    def add_move(self, move):
+        self.moves.append(move)
+
+    def clear_moves(self):
+        self.moves = []
