9d58766734a1cd869b9cdf6471ccd82aa2b883bf
Vince Memmo <vmemmo1@gmail.com>
2023-05-02 20:32:37 -0700

    work on plays

--- src/plays.py
+++ src/plays.py
@@ -39,22 +39,4 @@ class Game:
                         piece.texture_rect = img.get_rect(center=img_center)
                         surface.blit(img, piece.texture_rect)
 
-    def show_moves(self, surface):
-        if self.dragger.dragging:
-            piece = self.dragger.piece
-            for move in piece.moves:
-                color = '#808080'
-                x = move.final.col * SQSIZE + (SQSIZE // 2)
-                y = move.final.row * SQSIZE + (SQSIZE // 2)
-                rect = (move.final.col * SQSIZE, move.final.row * SQSIZE, SQSIZE, SQSIZE)
-                pygame.draw.circle(surface, color, (x, y), 20, 5)
 
-    def set_texture(self, size=80):
-        self.texture = os.path.join(
-            f'../assets/images/imgs-{size}px/{self.color}_{self.name}.png')
-
-    def add_move(self, move):
-        self.moves.append(move)
-
-    def clear_moves(self):
-        self.moves = []
