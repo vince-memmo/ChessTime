0d399ab1b85554cf1d0b08a05487c659482452ce
Vince Memmo <vmemmo1@gmail.com>
2023-05-04 21:23:37 -0700

    adding checks

--- src/plays.py
+++ src/plays.py
@@ -24,19 +24,6 @@ class Game:
                 pygame.draw.rect(surface, color, rect)
 
 
-    def show_pieces(self, surface):
-        for row in range(ROWS):
-            for col in range(COLS):
-
-                # print(row, col != 1, 0)
-                if self.board.squares[row][col].has_piece():
-                    piece = self.board.squares[row][col].piece
-
-                    if piece is not self.dragger.piece:
-                        piece.set_texture(size=80)
-                        img = pygame.image.load(piece.texture)
-                        img_center = col * SQSIZE + SQSIZE // 2, row * SQSIZE + SQSIZE // 2
-                        piece.texture_rect = img.get_rect(center=img_center)
-                        surface.blit(img, piece.texture_rect)
+   git add .
 
 
